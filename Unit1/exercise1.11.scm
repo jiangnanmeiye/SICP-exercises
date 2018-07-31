6484333eee5ecf8056e460074df9ff6f9eed113d
meiye <1486181677@qq.com>
2018-07-31 11:17:28 +0800

    exercise1.11.scm

---
+++ Unit1/exercise1.11.scm
@@ -0,0 +1,20 @@
+;;树形递归
+(define (f n)
+    (if (< n 3)
+        n
+       (+ (f (- n 1))
+       (* 2 (f (- n 2)))
+       (* 3 (f (- n 3))))))
+
+;;迭代计算过程
+(define (f n)
+    (f-iter 2 1 0 n))
+
+(define (f-iter a b c n)
+    (cond ((= n 0) c)
+        ((= n 1) b)
+        ((= n 2) a)
+        (else (f-iter (+ a (* 2 b) (* 3 c))
+        a
+        b
+        (- n 1)))))
