a6248b7581465aab7ce4225eddf664f1c69c30fa
meiye <1486181677@qq.com>
2018-08-17 21:06:22 +0800

    1.22

---
+++ Unit1/exercise/exercise1.22.scm
@@ -0,0 +1,36 @@
+(define (square x) (* x x))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+       ((divides? test-divisor n) test-divisor)
+       (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= (smallest-divisor n) n))
+
+(define (timed-prime-test n)
+    (newline)
+    (display n)
+    (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+    (if (prime? n)
+        (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time))
+
+(define (search-for-primes startNum)
+    (search-for-range-primes startNum (+ startNum 1000)))
+
+(define (search-for-range-primes startNum endNum)
+    (report-prime startNum)
+    (if (< startNum endNum)
+        (search-for-range-primes (+ startNum 1) endNum)))
